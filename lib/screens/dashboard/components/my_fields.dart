8f45bec818483393b50242f344b76907e0338f6a
Benjamin Mille <benjamin.mille@hotmail.fr>
2022-01-25 00:46:02 +0100

    test CI

--- lib/screens/dashboard/components/my_fields.dart
+++ lib/screens/dashboard/components/my_fields.dart
@@ -19,7 +19,7 @@ class MyFiles extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
-              "My Files",
+              "Tournament",
               style: Theme.of(context).textTheme.subtitle1,
             ),
             ElevatedButton.icon(
